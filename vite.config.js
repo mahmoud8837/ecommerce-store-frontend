2607fa713bd269692e98381de7345cc5052b7e89
mahmoud8837 <160542473+mahmoud8837@users.noreply.github.com>
2025-06-16 02:55:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api/": "https://e-commerce-store-backend-eta.vercel.app",
+      "/uploads/": "https://e-commerce-store-backend-eta.vercel.app",
+    },
+  },
+});
