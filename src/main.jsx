a05f7f43ab758b2e183b2fa6645766456cbdcbb4
mahmoud8837 <160542473+mahmoud8837@users.noreply.github.com>
2025-06-16 11:52:20 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -61,7 +61,7 @@ const router = createBrowserRouter(
         <Route path="dashboard" element={<AdminDashboard />} />
       </Route>
     </Route>
-  )
+  ), {basename: "/"}
 );
 
 createRoot(document.getElementById("root")).render(
